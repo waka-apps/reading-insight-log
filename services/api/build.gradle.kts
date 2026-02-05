066dfd454531e8569f28d9234d4daaf655b9b7be
waka-apps <waka.apps@gmail.com>
2026-02-05 23:29:52 +0900

    可読性向上のためapplication.propertiesをymlに変更。 開発環境でのcorsエラー対応

--- services/api/build.gradle.kts
+++ services/api/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("io.micronaut:micronaut-http-client")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    runtimeOnly("org.yaml:snakeyaml")
 }
 
 
